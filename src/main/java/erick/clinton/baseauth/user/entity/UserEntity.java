22fd854ccd0e476714f2559cd7a3c5db96b084e5
Erick Clinton <erickleite2009321@gmail.com>
2024-08-08 09:03:00 -0300

    feat: create get and setters

--- src/main/java/erick/clinton/baseauth/user/entity/UserEntity.java
+++ src/main/java/erick/clinton/baseauth/user/entity/UserEntity.java
@@ -17,7 +17,7 @@ public class UserEntity {
     @Column(unique = true, name = "username")
     private String username;
 
-    @Column(name = "username")
+    @Column(name = "password")
     private String password;
 
     @ManyToMany(cascade = CascadeType.ALL,fetch = FetchType.EAGER)
@@ -27,4 +27,36 @@ public class UserEntity {
             inverseJoinColumns = @JoinColumn(name = "role_id")
     )
     private Set<RoleEntity> roles;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<RoleEntity> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<RoleEntity> roles) {
+        this.roles = roles;
+    }
 }
