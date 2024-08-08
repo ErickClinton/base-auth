39d25348345a9314fbe5150af095136c3ce8b88b
Erick Clinton <erickleite2009321@gmail.com>
2024-08-08 08:06:21 -0300

    base structure

---
+++ src/main/java/erick/clinton/baseauth/BaseauthApplication.java
@@ -0,0 +1,13 @@
+package erick.clinton.baseauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/erick/clinton/baseauth/BaseauthApplicationTests.java
@@ -0,0 +1,13 @@
+package erick.clinton.baseauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
