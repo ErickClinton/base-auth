9ad25381e16112267a5c28b0f0b088d8a283f5ed
Erick Clinton <erickleite2009321@gmail.com>
2024-08-12 15:21:46 -0300

    feat: enable feign client

--- src/main/java/erick/clinton/baseauth/BaseauthApplication.java
+++ src/main/java/erick/clinton/baseauth/BaseauthApplication.java
@@ -2,8 +2,10 @@ package erick.clinton.baseauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class BaseauthApplication {
 
 	public static void main(String[] args) {
